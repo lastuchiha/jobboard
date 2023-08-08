0db1c25b6b7c42382be9c03f97fef4724b758aca
lastuchiha <tharun5a3@sasi.ac.in>
2023-08-08 21:50:39 +0530

    add: Add Jobs Model in jobs app

--- jobboard/jobs/models.py
+++ jobboard/jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    title = models.CharField(max_length=150)
+    job_url = models.URLField()
+    image_url = models.URLField("image")
+    posted_at = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.title
