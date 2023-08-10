9ed8d31d39a5686684e9d0def287c45ffe6f307c
lastuchiha <tharun5a3@sasi.ac.in>
2023-08-10 09:30:49 +0530

    add: Add slug field to uniquely identify the job

--- jobboard/jobboard/jobs/models.py
+++ jobboard/jobboard/jobs/models.py
@@ -3,9 +3,10 @@ from django.db import models
 
 class Job(models.Model):
     title = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True, db_index=True, max_length=150)
     job_url = models.URLField()
     image_url = models.URLField("image")
-    posted_at = models.DateTimeField()
+    posted_at = models.DateField()
 
     def __str__(self) -> str:
         return self.title
