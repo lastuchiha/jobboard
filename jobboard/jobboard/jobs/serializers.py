518186abff84824276a2c496711c9883b17fb07f
lastuchiha <tharun5a3@sasi.ac.in>
2023-08-11 10:39:41 +0530

    change: to_representation of posted_at of Job model

--- jobboard/jobboard/jobs/serializers.py
+++ jobboard/jobboard/jobs/serializers.py
@@ -6,3 +6,7 @@ class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = ("title", "image_url", "job_url", "posted_at")
+
+    def to_representation(self, instance):
+        instance.posted_at = instance.posted_at.strftime("%d %b, %Y")
+        return super().to_representation(instance)
